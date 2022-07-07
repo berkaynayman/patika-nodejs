b4a734d2f0bc257fb3491bc2c2eeff469e685256
berkaynayman <berkaynayman4@gmail.com>
2022-07-07 20:54:19 +0300

    Clean Blog Case 2 Public folder created. Views folder created. Partial folder created. EJS module added.

--- chapter4/cleanBlog1/app.js
+++ chapter4/cleanBlog1/app.js
@@ -1,19 +1,23 @@
 const express = require("express")
 const app = express()
 
-const port = 3000
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs')
+
+// MIDDLEWARES
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-    const blog = [
-        { id: 1, title: "Blog title 1", description: "Blog description 1" },
-        { id: 2, title: "Blog title 2", description: "Blog description 2" },
-        { id: 3, title: "Blog title 3", description: "Blog description 3" },
-        { id: 4, title: "Blog title 4", description: "Blog description 4" }
-    ]
-
-    res.send(blog)
+    res.render('index')
+})
+app.get("/about", (req, res) => {
+    res.render('about')
+})
+app.get("/add_post", (req, res) => {
+    res.render('add_post')
 })
 
+const port = 3000
 app.listen(port, () => {
     console.log(`${port} unda çalışıyor.`);
 })
